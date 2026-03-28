8a80481edef2f1d4e7e598b1782ca9c746519e58
Alus <avp3085@gmail.com>
2026-03-28 14:21:33 +0100

    commit create postgres db

--- learn_studio/settings.py
+++ learn_studio/settings.py
@@ -72,11 +72,21 @@ WSGI_APPLICATION = "learn_studio.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'learn_studio_db',
+        'USER': 'learn_admin',
+        'PASSWORD': 'password12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+#DATABASES = {
+    #"default": {
+    #    "ENGINE": "django.db.backends.sqlite3",
+    #    "NAME": BASE_DIR / "db.sqlite3",
+    #}
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
